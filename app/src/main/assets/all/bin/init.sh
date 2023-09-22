ad3a780553fa74b9eed987b425fb7be311652c2d
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-09-22 19:51:32 +0800

    update

--- app/src/main/assets/all/bin/init.sh
+++ app/src/main/assets/all/bin/init.sh
@@ -3,9 +3,9 @@ echo "### init\n"
 
 if test -d /sdcard/luomor
 then
-  echo "### exist\n"
+  echo "### exist /sdcard/luomor\n"
 else
-  echo "### create dir\n"
+  echo "### create dir /sdcard/luomor\n"
   mkdir /sdcard/luomor
 fi
 
